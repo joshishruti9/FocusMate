123339f239e4da2f70d3e1592d80b4cc3b189fc9
Anthony Ly <aly@seattleu.edu>
2025-10-27 14:23:51 -0700

    Update basic-shop branch with local changes

--- frontend/frontend-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CreateTaskComponent } from './components/create-task/create-task.component';
-
-export const routes: Routes = [ 
-    { 
-    path: '', 
-    component: CreateTaskComponent 
-  }
-];
--- task-service/src/routes/task.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createTask, getAllTasks } from "../controllers/task.controller";
-
-const router = Router();
-
-router.post("/", createTask);
-router.get("/", getAllTasks);
-
-export default router;
