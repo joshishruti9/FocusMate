4ccfa037195b27305da2b5bbcb5d2f91c0dba985
joshishruti9 <joshishruti009@gmail.com>
2025-11-12 22:25:28 -0800

    Refactor routing configuration to include Shop and ViewTasks components

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -1,12 +1,16 @@
 // src/app/app.routes.ts
-import { Routes } from '@angular/router';
+import { Route, Routes } from '@angular/router';
 import { HomeComponent } from './components/HomeComponent/home.component';
 import { TaskComponent } from './components/TaskComponent/task.component';
+import { ShopComponent } from './components/ShopComponent/shop.component';
+import { ViewTasksComponent } from './components/ViewTaskComponent/viewTask.component';
 
 export const routes: Route[] = [
   { path: '', component: HomeComponent }, // default route
   { path: 'shop', component: ShopComponent },   // shop route
-  { path: 'task', component: TaskComponent }  // task route
+  { path: 'task', component: TaskComponent },  // task route
+  {path: 'viewTasks', component: ViewTasksComponent},  // view tasks route
+  { path: '**', redirectTo: 'home' } // fallback route
 ];
 
 
